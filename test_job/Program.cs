94027b2ed9190edffc74997f9eddb9cbb30d5b8c
cheniro <jml.cheniro.1d13@gmail.com>
2020-04-26 00:39:33 +0400

    third commit

--- test_job/Program.cs
+++ test_job/Program.cs
@@ -4,37 +4,67 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace test_job
 {
-    
+    class Program
+    {
+        static char[,] letters = {{'А', 'П', 'К', 'А', 'Н' },
+                                  {'Б', 'З', 'А', 'Б', 'В' },
+                                  {'Г', 'П', 'Ы', 'А', 'У' },
+                                  {'Х', 'Ш', 'И', 'И', 'Н' }};
+
+        static void Main(string[] args)
+        {
+            string option = "";
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Выберите опцию:");
+                Console.WriteLine("Задание с массивом - 1\n" +
+                                  "Задание с классом - 2\n" +
+                                  "Выход - Любой другой символ");
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1": ArraySort.sorting(letters); break;
+                    case "2": CategoryReader.createTree(); break;
+                    default: exit = true; break;
+                }
+
+            }
+        }
+    }
+
     public class Tree
     {
         string name;
         Tree parent;
         List<Tree> child = new List<Tree>();
-        public Tree(string name, Tree parent = null)
+        public Tree(string name)
         {
             this.name = name;
-            if(parent != null)
-            {
-                this.parent = parent;
-                parent.child.Add(this);
-            }
         }
         
-        public string GetStruct()
+        public void addParent(Tree parent)
+        {
+            this.parent = parent;
+            parent.child.Add(this);
+        }
+
+        public string getStruct()
         {
             string ans = "Родителя нет!\n";
             if (parent != null)
                 ans = string.Format("Родительская категория: {0}\n", parent.name);
-            ans += GetChild(1);
+            ans += getChild(1);
             return ans;
         }
 
-        public string GetChild(int n)
+        public string getChild(int n)
         {
             string ans = printSymbols(n) + string.Format("{0}\n", name);
             if (child.Count > 0)
                 foreach (Tree ch in child)
-                    ans += ch.GetChild(n + 1);
+                    ans += ch.getChild(n + 1);
             return ans;
         }
 
@@ -65,7 +95,7 @@ namespace test_job
                 if (rowValue != null)
                     data.Add(rowValue);
                 else
-                    break;///////
+                    break;
             }
 
             workbook.Close(false);
@@ -80,31 +110,31 @@ namespace test_job
                 for (int i = 0; i < temp.Length; i++)
                     if (temp[i] != "" && !dict.ContainsKey(temp[i]))
                     {
-                        Tree newItem;
-                        if (i == 0)
-                            newItem = new Tree(temp[i]);
-                        else
-                            newItem = new Tree(temp[i], dict[temp[i - 1]]);
+                        Tree newItem = new Tree(temp[i]);
+
+                        if (i > 0)
+                            newItem.addParent(dict[temp[i - 1]]);
 
                         keys.Add(temp[i]);
                         dict.Add(temp[i], newItem);
                     }
             }
-            string get_c = "";
+            
+            string selectVal = "";
             do
             {
-                Console.WriteLine("");
+                Console.WriteLine("\nВыберите категорию:");
                 for (int i = 0; i < keys.Count; i++)
                     Console.WriteLine(string.Format("{0} - {1}", keys[i], i));
 
                 Console.WriteLine("Выход - q\n");
-                get_c = Console.ReadLine();
+                selectVal = Console.ReadLine();
 
-                if (Int32.TryParse(get_c, out int indx) && indx < keys.Count - 1 && indx >= 0)
-                    Console.Write(dict[keys[indx]].GetStruct());
-                else if (get_c != "q")
+                if (Int32.TryParse(selectVal, out int index) && index < keys.Count && index >= 0)
+                    Console.Write(dict[keys[index]].getStruct());
+                else if (selectVal != "q")
                     Console.WriteLine("Неверныее входные данные!");
-            } while (get_c != "q");
+            } while (selectVal != "q");
         }
     }
 
@@ -112,70 +142,65 @@ namespace test_job
     {
         public static void sorting(char[,] letters)
         {
-            cw(ref letters);
-            int indx_min = -1;
+            printArray(ref letters);
+            int indexMin = -1;
             for (int i = 0; i < letters.GetLength(1) - 1; i++)
             {
-                indx_min = ind_min(ref letters, i);
-                if (letters[0, indx_min] == letters[0, i] && indx_min != i)
-                    indx_min = ind_min2(ref letters, i, indx_min);
+                indexMin = findIndexMinColumn(ref letters, i);
+                if (letters[0, indexMin] == letters[0, i] && indexMin != i)
+                    indexMin = findIndexMinValue(ref letters, i, indexMin);
 
-                if (indx_min != i)
-                {
-                    swap(ref letters, i, indx_min);
-                    cw(ref letters);
-                }
+                if (indexMin != i)
+                    swap(ref letters, i, indexMin);
             }
             Console.WriteLine("\nРезультат:\n");
-            cw(ref letters);
+            printArray(ref letters);
 
         }
 
-        static void swap(ref char[,] let, int f, int s)
+        static void swap(ref char[,] letters, int first, int second)
         {
             int diff = 0;
-            for (int i = 0; i < let.GetLength(0); i++)
+            for (int i = 0; i < letters.GetLength(0); i++)
             {
-                diff = let[i, f] - let[i, s];
-                let[i, f] = (char)(let[i, f] - diff);
-                let[i, s] = (char)(let[i, s] + diff);
+                diff = letters[i, first] - letters[i, second];
+                letters[i, first] = (char)(letters[i, first] - diff);
+                letters[i, second] = (char)(letters[i, second] + diff);
             }
         }
 
-        static int ind_min(ref char[,] arr, int st_ind)
+        static int findIndexMinColumn(ref char[,] letters, int startIndex)
         {
-            int ind = st_ind;
-            for (int i = st_ind + 1; i < arr.GetLength(1); i++)
-                if (arr[0, ind] > arr[0, i])
-                    ind = i;
-                else if (arr[0, ind] == arr[0, i])
+            int index = startIndex;
+            for (int i = startIndex + 1; i < letters.GetLength(1); i++)
+                if (letters[0, index] > letters[0, i])
+                    index = i;
+                else if (letters[0, index] == letters[0, i])
                 {
-                    ind = i;
+                    index = i;
                     break;
                 }
-            return ind;
+            return index;
         }
 
-        static int ind_min2(ref char[,] arr, int f, int s)
+        static int findIndexMinValue(ref char[,] letters, int first, int second)
         {
-            int ind = f;
-            for (int i = 1; i < arr.GetLength(0); i++)
-                if (arr[i, f] > arr[i, s])
+            int index = first;
+            for (int i = 1; i < letters.GetLength(0); i++)
+                if (letters[i, first] > letters[i, second])
                 {
-                    ind = s;
+                    index = second;
                     break;
                 }
-            return ind;
+            return index;
         }
 
-        static void cw(ref char[,] arr)
+        static void printArray(ref char[,] letters)
         {
-            for (int t = 0; t < arr.GetLength(0); t++)
+            for (int t = 0; t < letters.GetLength(0); t++)
             {
-                for (int i = 0; i < arr.GetLength(1); i++)
-                {
-                    Console.Write(arr[t, i] + " ");
-                }
+                for (int i = 0; i < letters.GetLength(1); i++)
+                    Console.Write(letters[t, i] + " ");
                 Console.WriteLine("");
             }
             Console.WriteLine("");
@@ -183,33 +208,5 @@ namespace test_job
     }
 
 
-    class Program
-    {
-        static char[,] letters = {{'А', 'П', 'К', 'А', 'Н' },
-                                  {'Б', 'З', 'А', 'Б', 'В' },
-                                  {'Г', 'П', 'Ы', 'А', 'У' },
-                                  {'Х', 'Ш', 'И', 'И', 'Н' }};
-
-
-        static void Main(string[] args)
-        {
-            string n_test = "";
-            do
-            {
-                Console.WriteLine("Массив - 1\nКласс - 2\nИначе - Выход");
-                n_test = Console.ReadLine();
-                if (n_test == "1")
-                    ArraySort.sorting(letters);
-                else if (n_test == "2")
-                    CategoryReader.createTree();
-                
-            } while (n_test == "1" || n_test == "2");
-            Console.WriteLine("Нажмите любую кнопку для выхода.");
-            Console.ReadKey();
-        }
-
-        
-
-        
-    }
+    
 }
